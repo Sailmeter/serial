16994a1d50a462a59a215f72aa8e4c1732b26dd8
Mark Allen <mark@markcallen.com>
2015-04-27 20:48:56 -0700

    Added GPS based upon speed and direction

---
+++ intersect.js
@@ -0,0 +1,57 @@
+// from http://jsfiddle.net/justin_c_rounds/Gd2S2/
+
+module.exports = { 
+  checkLineIntersection: function(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY) {
+    // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
+    var denominator, a, b, numerator1, numerator2, result = {
+        x: null,
+        y: null,
+        onLine1: false,
+        onLine2: false
+    };
+    denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
+    if (denominator == 0) {
+        return result;
+    }
+    a = line1StartY - line2StartY;
+    b = line1StartX - line2StartX;
+    numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
+    numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
+    a = numerator1 / denominator;
+    b = numerator2 / denominator;
+
+    // if we cast these lines infinitely in both directions, they intersect here:
+    result.x = line1StartX + (a * (line1EndX - line1StartX));
+    result.y = line1StartY + (a * (line1EndY - line1StartY));
+/*
+        // it is worth noting that this should be the same as:
+        x = line2StartX + (b * (line2EndX - line2StartX));
+        y = line2StartX + (b * (line2EndY - line2StartY));
+        */
+    // if line1 is a segment and line2 is infinite, they intersect if:
+    if (a > 0 && a < 1) {
+        result.onLine1 = true;
+    }
+    // if line2 is a segment and line1 is infinite, they intersect if:
+    if (b > 0 && b < 1) {
+        result.onLine2 = true;
+    }
+    // if line1 and line2 are segments, they intersect if both of the above are true
+    return result;
+  },
+
+  getLine: function(point, angle, distance) {
+    if (distance == null) {
+      distance = Math.abs(point.x * 0.1);
+    }
+    var point2 = {};
+    point2.x = point.x + Math.cos(angle*Math.PI/180) * distance;
+    point2.y = point.y + Math.sin(angle*Math.PI/180) * distance;
+
+    return point2;
+  },
+
+  getDistance: function(point1, point2) {
+    return Math.sqrt( (point2.x-=point1.x)*point2.x + (point2.y-=point1.y)*point2.y );
+  }
+}
--- nmeacreator.js
+++ nmeacreator.js
@@ -1,5 +1,9 @@
 var utils = require("./utils.js");
 var fs = require("fs");
+var ivector = require("./intersect");
+
+var lat =  49.2635844;
+var lon = -123.1821251;
 
 
 while (1) {
@@ -16,4 +20,13 @@ while (1) {
   sentenance = "$VWVHW," + direction.toFixed(2) + ",M," + (direction-20).toFixed(2) + ",T," + speed.toFixed(2) + "," + (speed*1.852).toFixed(2);
   var checksum = utils.computeChecksum(sentenance);
   console.log(sentenance + checksum + "\r");   
+
+  var latlon = ivector.getLine({x: lat, y: lon}, direction, speed/60/3600);
+  lat = latlon.x;
+  lon = latlon.y;
+
+  sentenance = "$GPGGA,002500.060," + lat + ",N," + lon + ",W,0,00,,0.0,M,0.0,M,,0000";
+  var checksum = utils.computeChecksum(sentenance);
+  console.log(sentenance + checksum + "\r");   
+
 }
